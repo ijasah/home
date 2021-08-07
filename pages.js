574607924fc7ce6a2f719634bc565092c8b55c24
IJAS A H <41640210+ijasah@users.noreply.github.com>
2021-08-07 10:09:24 +0530

    Update pages.js

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/ijasah/home.github.io.git";
+const repoURL = "https://github.com/ijasah/home/";
 
 ghpages.publish(
   pathname,
